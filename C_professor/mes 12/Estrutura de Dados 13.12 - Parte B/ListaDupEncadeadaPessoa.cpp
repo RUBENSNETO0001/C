8cd694750a8a9cc608ff1edd94083d5af888c6a9
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-12-16 17:49:55 -0300

    16do12

--- C_professor/mes 12/Estrutura de Dados 13.12 - Parte B/ListaDupEncadeadaPessoa.cpp
+++ C_professor/mes 12/Estrutura de Dados 13.12 - Parte B/ListaDupEncadeadaPessoa.cpp
@@ -3,28 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Estrutura pessoa - struct - lista duplamente encadeada
-typedef struct pessoa {
+// Estrutura pessoa - struct - lista duplamente encadeada
+typedef struct pessoa
+{
 	int id;
 	char nome[80];
 	int idade;
 	float peso;
-	struct pessoa* anterior;
-	struct pessoa* proximo;
-} Pessoa; //Fim da struct pessoa com typedef
+	struct pessoa *anterior;
+	struct pessoa *proximo;
+} Pessoa; // Fim da struct pessoa com typedef
 
-//Função criarLista()
-Pessoa* criarLista() {
+// Funï¿½ï¿½o criarLista()
+Pessoa *criarLista()
+{
 	return NULL;
-}//Fim da função criarLista()
+} // Fim da funï¿½ï¿½o criarLista()
 
-//Função criarNo() - apenas cria um novo nó
-Pessoa* criarNo(int id, char* nome, int idade, float peso) {
-	Pessoa* novaPessoa = (Pessoa*)malloc(sizeof(Pessoa));
-	if (!novaPessoa) {
-		printf("Erro ao alocar memória! Não será possível inserir novo elemento na lista!\n");
+// Funï¿½ï¿½o criarNo() - apenas cria um novo nï¿½
+Pessoa *criarNo(int id, char *nome, int idade, float peso)
+{
+	Pessoa *novaPessoa = (Pessoa *)malloc(sizeof(Pessoa));
+	if (!novaPessoa)
+	{
+		printf("Erro ao alocar memÃ³ria! NÃ£o serÃ¡ possÃ­vel inserir novo elemento na lista!\n");
 		exit(1);
-	} 
+	}
 	novaPessoa->id = id;
 	strcpy(novaPessoa->nome, nome);
 	novaPessoa->idade = idade;
@@ -32,45 +36,152 @@ Pessoa* criarNo(int id, char* nome, int idade, float peso) {
 	novaPessoa->anterior = NULL;
 	novaPessoa->proximo = NULL;
 	return novaPessoa;
-}//Fim da função criarNo()
+} // Fim da funï¿½ï¿½o criarNo()
 
-//Função listaVazia()
-int listaVazia(Pessoa* lista) {
+// Funï¿½ï¿½o listaVazia()
+int listaVazia(Pessoa *lista)
+{
 	return lista == NULL;
-}//Fim da função listaVazia()
+} // Fim da funï¿½ï¿½o listaVazia()
 
-//Função inserirInicio() - insere elemento no início da lista duplamente encadeada
-Pessoa* inserirInicio(Pessoa* lista, int id, char* nome, int idade, float peso) {
-	Pessoa* novaPessoa = criarNo(id, nome, idade, peso);
+// Funï¿½ï¿½o inserirInicio() - insere elemento no inï¿½cio da lista duplamente encadeada
+Pessoa *inserirInicio(Pessoa *lista, int id, char *nome, int idade, float peso)
+{
+	Pessoa *novaPessoa = criarNo(id, nome, idade, peso);
 	novaPessoa->proximo = lista;
-	if (lista != NULL) {
+	if (lista != NULL)
+	{
 		lista->anterior = novaPessoa;
 	}
 	return novaPessoa;
-}//Fim da função inserirInicio()
+} // Fim da funï¿½ï¿½o inserirInicio()
 
-//Função inserirFim()
-Pessoa* inserirFim(Pessoa* lista, int id, char* nome, int idade, float peso) {
-	Pessoa* novaPessoa = criarNo(id, nome, idade, peso);
-	if (lista == NULL) {
+// Funï¿½ï¿½o inserirFim()
+Pessoa *inserirFim(Pessoa *lista, int id, char *nome, int idade, float peso)
+{
+	Pessoa *novaPessoa = criarNo(id, nome, idade, peso);
+	if (lista == NULL)
+	{
 		return novaPessoa;
 	}
-	Pessoa* aux = lista;
-	while (aux->proximo != NULL) {
+	Pessoa *aux = lista;
+	while (aux->proximo != NULL)
+	{
 		aux = aux->proximo;
 	}
 	aux->proximo = novaPessoa;
 	novaPessoa->anterior = aux;
 	return lista;
-}//Fim da função inserirFim()
+} // Fim da funï¿½ï¿½o inserirFim()
 
-//1 - Inserir no meio da lista????
-//2 - Buscar elemento - nó
-//3 - Remover elemento - nó
-//4 - Liberar lista
-//5 - Imprimir na ordem direta
-//6 - Imprimir na ordem inversa
+// procedimento de colocar no meio da lista
+Pessoa *inserirMeio(Pessoa *lista, int id, char *nome, int idade, float peso)
+{
+	Pessoa *novaPessoa = criarNo(id, nome, idade, peso);
+	if (lista == NULL)
+	{
+		return novaPessoa;
+	}
 
-//Montar a main()
+	Pessoa *aux = lista;
+	while (aux->proximo != NULL)
+	{
+		aux = aux->proximo;
+	}
+	aux->proximo = aux;
+	aux == aux;
 
+	return lista;
+} // fim do procedimento de colocar no meio da lista
 
+// buscar por id
+Pessoa *buscarPorId(Pessoa *lista, int idBuscar)
+{
+	Pessoa *aux = lista;
+	while (aux!= NULL){
+		if(aux->id == idBuscar)
+		return aux;
+		aux = aux->proximo;
+	}
+	return NULL;
+}//fim de buscar por id
+
+// remover elemento- nÃ³
+Pessoa* removerElementono(Pessoa *lista, int id){
+	Pessoa* aux = buscarPorId(lista,id);
+
+	if(aux == NULL){
+		printf("\n\n\nNÃ£o a essa pessoa na lista");
+		return lista;
+	}
+	if(aux->anterior != NULL){
+		aux->anterior->proximo = aux->proximo;
+	}
+	else{
+		lista = lista->proximo;
+	}
+	if(aux->proximo!=NULL){
+		aux->proximo->anterior = aux-> anterior;
+	}
+	free(aux);
+
+	printf("\n\n\nRegistro /pessoa com id %d removido com sucesso!!", id);
+	return lista;
+}
+// fim de remover elemento- nÃ³
+
+//liberar lista
+Pessoa* liberarLista(Pessoa* lista, int id){
+	Pessoa* aux = lista;
+	while(aux != NULL){
+		Pessoa* proximoPessoa = aux->proximo;
+		free(aux);
+		aux = proximoPessoa;
+	}
+	printf("\n\n\nA lista liberada com sucesso!!\n");
+	return NULL;
+}
+
+// fim da liberarlista()
+
+// imprimir lista direta
+void imprimirListaDireta(Pessoa *lista)
+{
+	if (listaVazia(lista))
+	{
+		printf("\n\n\nA lista estÃ¡ vazia!!");
+		return;
+	}
+	Pessoa *aux = lista;
+	printf("\n\n\nLista na ordem direta:\n");
+	while (aux != NULL)
+	{
+		printf("\n\nId: %d.\nNome: %s., \nIdade: %d. , \nPeso(kg): %.2f.\n\n", aux->nome, aux->idade, aux->peso);
+		aux = aux->proximo;
+	}
+} // fim de imprimir lista direta
+
+// imprimir lista inversar
+void imprimirListaInversa(Pessoa *lista)
+{
+	if (listaVazia(lista))
+	{
+		printf("\n\n\nA lista esta vazia!!");
+		return;
+	}
+	Pessoa *aux = lista;
+	while (aux->proximo != NULL)
+	{
+		aux = aux->proximo;
+	}
+	printf("\n\n\nLista na ordem Inversa: ");
+	while (aux != NULL)
+	{
+		printf("\n\nId: %d.\nNome: %s., \nIdade: %d. , \nPeso(kg): %.2f.\n\n", aux->nome, aux->idade, aux->peso);
+		aux = aux->anterior;
+	}
+} // fim de imprimir lista inversar
+
+// 1 - Inserir no meio da lista????
+
+// Montar a main()
